7f7c26d1c6a77bdb853e59bdbee86b77c7173ef7
Akshar Patel <aksharcommit@gmail.com>
2024-03-02 20:20:46 -0500

    Add repo_create

--- libwyag.py
+++ libwyag.py
@@ -82,7 +82,7 @@ def repo_file(repo, *path, mkdir=False):
         return repo_path(repo, *path)
 
 def repo_dir(repo, *path, mkdir=False):
-    """Same as repo_path but mkdir *path if absent if mkdir"""
+    """Same as repo_path but mkdir *path if absent if mkdir, mkdir must be passed by name"""
 
     path = repo_path(repo, *path)
 
@@ -98,3 +98,35 @@ def repo_dir(repo, *path, mkdir=False):
     else:
         return None
 
+def repo_create(path):
+    """Create a new repository at path"""
+
+    repo = GitRepository(path, True)
+
+    #Make sure the path either doesn't exist or is an empty dir
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception ("%s is not a directory!" % path)
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+    
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads" mkdir=True)
+
+    #.git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description to name the repository.\n")
+
+    #.git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+    
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
