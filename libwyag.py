87b18279aa30c95364d5e8009e02251b6d5dd9dc
Akshar Patel <aksharcommit@gmail.com>
2024-03-02 20:46:25 -0500

    Add repo_find

--- libwyag.py
+++ libwyag.py
@@ -162,3 +162,24 @@ argsp.add_argument("path",
 def cmd_init(args):
     repo_create(args.path)
 
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path,"git")):
+        return GitRepository(path)
+    
+    # If we haven't returned, recurse in parent
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    """
+    Base Case
+    If parent==path, then path is root
+    """
+    if parent == path:
+        if required:
+            raise Exception("No git directory")
+        else:
+            return None
+    
+    # Recursive case
+    return repo_find(parent, required)
